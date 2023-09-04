1e49378b36f9899bbc20e31acccc5243c2a17064
Mehmet Faruk Andaç <83770786+mehmetfarukandac@users.noreply.github.com>
2023-09-04 16:41:14 +0300

    Logo
    Logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -74,11 +74,7 @@ export default function Header() {
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
-                            <img
-                                className="w-auto h-8"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                alt=""
-                            />
+                            <Logo />
                         </a>
                         <button
                             type="button"
